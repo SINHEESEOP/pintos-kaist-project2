fa755f8be649213d08bc7ba2f6f320cb15032eaa
SINHEESEOP <shfurwnd97@gmail.com>
2024-11-13 19:28:38 +0900

    args.c 파일 번역

--- tests/userprog/args.c
+++ tests/userprog/args.c
@@ -1,7 +1,7 @@
-/* Prints the command-line arguments.
-   This program is used for all of the args-* tests.  Grading is
-   done differently for each of the args-* tests based on the
-   output. */
+/* 명령줄 인자들을 출력합니다.
+   이 프로그램은 모든 args-* 테스트에 사용됩니다.
+   각각의 args-* 테스트마다 출력을 기준으로 
+   다르게 채점됩니다. */
 
 #include "tests/lib.h"
 
@@ -13,16 +13,16 @@ main (int argc, char *argv[])
   test_name = "args";
 
   if (((unsigned long long) argv & 7) != 0)
-    msg ("argv and stack must be word-aligned, actually %p", argv);
+    msg ("argv와 스택은 워드 정렬되어야 합니다. 실제 값: %p", argv);
 
-  msg ("begin");
+  msg ("시작");
   msg ("argc = %d", argc);
   for (i = 0; i <= argc; i++)
     if (argv[i] != NULL)
       msg ("argv[%d] = '%s'", i, argv[i]);
     else
       msg ("argv[%d] = null", i);
-  msg ("end");
+  msg ("종료");
 
   return 0;
 }
